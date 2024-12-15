559eebc9cf5ca384f3ac36bc8b25a4105e921da2
shruti varshney <shrutivarshney611@gmail.com>
2024-12-15 14:24:42 +0530

    update the css

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Appointment from "./Pages/Appointment";
 function App() {
   return (
     <div className="App">
-      <Router basename="/Health-Care">
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/legal" element={<Legal />} />
